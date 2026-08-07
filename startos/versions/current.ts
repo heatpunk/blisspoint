c4c4f4359675e79952a308ef2059772fa45bbc1c
heatpunk <heatpunk@users.noreply.github.com>
2026-08-07 15:56:23 +0000

    fix: clean release build trigger for 0.6.0

--- startos/versions/current.ts
+++ startos/versions/current.ts
@@ -3,7 +3,7 @@ import { IMPOSSIBLE, VersionInfo } from '@start9labs/start-sdk'
 export const current = VersionInfo.of({
   version: '0.6.0:0',
   releaseNotes: {
-    en_US: 'Fix syntax typo in miners store import.',
+    en_US: 'Fix LAN scanning in StartOS/Umbrel Docker containers.',
   },
   migrations: {
     up: async ({ effects }) => {},
